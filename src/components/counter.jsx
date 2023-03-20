839c17da2a170720b1a55a44459a70c6dae43738
Vladreacteducation <vladpopovichjob2022@gmail.com>
2023-03-20 22:37:37 +0200

    20.03.23

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -21,26 +21,37 @@ const Counter = () => {
   };
 
   const handleTagChange = (id) => {
-    // setTags(["tag4", "tag5"]);
     setTags((prevState) => prevState.filter((tag) => tag !== id));
     // console.log(id);
   };
   // return function
+
+  const renderTags = () => {
+    // if (tag.length === 0) return "No tags";
+    return (
+      tag.length !== 0 &&
+      tag.map((tag) => (
+        <li
+          key={tag}
+          className="btn btn-primary btn-sm m-2"
+          onClick={() => handleTagChange(tag)}
+        >
+          {tag}
+        </li>
+      ))
+    );
+  };
+
+  if (setTags.length !== 0) {
+    return <ul>{renderTags()}</ul>;
+  }
+
   return (
     <>
       <div>
-        <ul>
-          {tag.map((tag) => (
-            <li
-              key={tag}
-              className="btn btn-primary btn-sm m-2"
-              onClick={() => handleTagChange(tag)}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
+        <ul>{renderTags()}</ul>
       </div>
+
       <span className={getbageClasses()}>{fomatCount()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
